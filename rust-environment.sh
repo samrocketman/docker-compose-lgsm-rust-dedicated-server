d40a6c6a0f79911287020d3466db4a70fd282a2a
Sam Gleske <sam.mxracer@gmail.com>
2021-01-18 12:28:42 -0500

    initial custom map code and rust-environment file

--- rust-environment.sh
+++ rust-environment.sh
@@ -1,2 +1,2 @@
 # uncomment this to enable EAC ()
-#export ENABLE_RUST_EAC=1
+#ENABLE_RUST_EAC=1
--- utils/custom-rust-server.sh
+++ utils/custom-rust-server.sh
@@ -6,7 +6,9 @@
 
 set -ex
 
-[ ! -r /tmp/rust-docker-environment.sh ] || source /tmp/rust-docker-environment.sh
+[ ! -r /rust-environment.sh ] || source /rust-environment.sh
+
+export ENABLE_RUST_EAC
 
 function rand_password() {
   tr -dc -- '0-9a-zA-Z' < /dev/urandom | head -c12;echo
@@ -23,6 +25,26 @@ server_cfg=serverfiles/server/rustserver/cfg/server.cfg
 if [ -z "${ENABLE_RUST_EAC:-}" ]; then
   grep -F -- server.secure "$server_cfg" || echo server.secure 0 >> "$server_cfg"
   grep -F -- server.encryption "$server_cfg" || echo server.encryption 0 >> "$server_cfg"
+else
+  sed -i '/^ *server\.secure/d' "$server_cfg"
+  sed -i '/^ *server\.encryption/d' "$server_cfg"
+fi
+
+# Custom Map Support
+function start_custom_map_server() (
+  cd /custom-maps/
+  python -m SimpleHTTPServer &
+)
+function get_custom_map_url() {
+  local base_url=http://localhost:8000/
+  until curl -sIfLo /dev/null "$base_url"; do sleep 1; done
+  local map_url="$(curl -sfL "$base_url" | grep -o 'href="[^"]\+.map"' | sed 's/.*"\([^"]\+\)"/\1/' | head -n1)"
+  echo "${base_url}${map_url}"
+}
+if ls -1 /custom-maps/*.map &> /dev/null; then
+  # custom map found so disabling map settings.
+  start_custom_map_server
+  export CUSTOM_MAP_URL="$(get_custom_map_url)"
 fi
 
 if [ ! -f rcon_pass ]; then

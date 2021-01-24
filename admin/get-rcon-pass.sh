3ea2ffe515d80e09ca73c76a16d9516df1a40d24
Sam Gleske <sam.mxracer@gmail.com>
2021-01-24 12:21:02 -0500

    Note more rcon web clients

--- admin/get-rcon-pass.sh
+++ admin/get-rcon-pass.sh
@@ -4,5 +4,8 @@ echo -n 'RCON password: '
 docker-compose exec -T lgsm cat rcon_pass
 
 echo '
-Visit http://facepunch.github.io/webrcon and connect to 127.0.0.1:28016 with
-this password.'
+Visit one of the following web RCON clients:
+
+- http://facepunch.github.io/webrcon address 127.0.0.1:28016
+- http://rcon.io/login address 127.0.0.1 port 28016
+'

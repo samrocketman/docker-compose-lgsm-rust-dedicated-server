5f2886abf9e60ee08f1653232d6280cf8311f4f4
Sam Gleske <sam.mxracer@gmail.com>
2021-02-05 18:28:54 -0500

    regenerate map compatibility with cron

--- admin/regenerate-map.sh
+++ admin/regenerate-map.sh
@@ -1,14 +1,25 @@
 #!/bin/bash
 
-cat <<'EOF'
+# set the working directory to repository root
+# only when this script is called by full path; e.g. from cron job
+if grep '^/' <<<  "$0" > /dev/null; then
+  echo "Changing working directory to: ${0%admin/*}"
+  cd "${0%admin/*}"
+fi
+
+if [ -n "${1:-}" ]; then
+  echo 'Skipping user prompt.'
+else
+  cat <<'EOF'
 WARNING: This is a permanent action.
 All maps will be destroyed including backups and a new map will be generated.
 EOF
-read -erp 'Do you wish to delete the current map? (y/N) ' response
+  read -erp 'Do you wish to delete the current map? (y/N) ' response
 
-if [ ! "$response" = y -a ! "$response" = Y ]; then
-  echo 'Operation aborted.  Respond "y" next time if you want to proceed.'
-  exit
+  if [ ! "$response" = y -a ! "$response" = Y ]; then
+    echo 'Operation aborted.  Respond "y" next time if you want to proceed.'
+    exit
+  fi
 fi
 
 docker-compose exec -T lgsm \

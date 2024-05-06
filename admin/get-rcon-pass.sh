deae9527fad64b4177f704db8940a55136c904ab
Timo <43761260+clemone210@users.noreply.github.com>
2024-05-06 21:29:11 +0200

    Update get-rcon-pass.sh
    replaced "docker-compose" with "docker compose"

--- admin/get-rcon-pass.sh
+++ admin/get-rcon-pass.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 echo -n 'RCON password: '
-docker-compose exec -T lgsm cat rcon_pass 2> /dev/null || (
+docker compose exec -T lgsm cat rcon_pass 2> /dev/null || (
   # Could not find rcon random password file so falling back to auto detection.
-  docker-compose exec -T lgsm pgrep RustDedicated | \
-  xargs -n1 -I'{}' -- docker-compose exec -T lgsm cat '/proc/{}/cmdline' | \
+  docker compose exec -T lgsm pgrep RustDedicated | \
+  xargs -n1 -I'{}' -- docker compose exec -T lgsm cat '/proc/{}/cmdline' | \
   tr '\0' '\n' | \
   awk '$1 == "+rcon.password" { x="1"; next}; x == "1" {print $0; exit}'
 )
